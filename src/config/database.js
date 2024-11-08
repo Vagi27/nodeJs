395a9802147ead88e6837c510c313e2a334e53f3
Vagish <Vagishmlk@gmail.com>
2024-11-08 06:51:43 +0530

    fixed connection string to make a new DB devTinder

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 
 
 const dbConnect=async()=>{
-    await mongoose.connect("mongodb+srv://vagishmlk:1lprUWFkzNU5rzj9@malikcluster.iyvwq.mongodb.net/?retryWrites=true&w=majority&appName=malikCluster/devTinder");
+    await mongoose.connect("mongodb+srv://vagishmlk:1lprUWFkzNU5rzj9@malikcluster.iyvwq.mongodb.net/devTinder?retryWrites=true&w=majority&appName=malikCluster/");
 }
 
 module.exports=dbConnect;
