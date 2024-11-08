97b3b5ff4741498674292b3d4505d6534c66b710
Vagish <Vagishmlk@gmail.com>
2024-11-08 06:54:48 +0530

    used model instance to create and save user details into the user collection after automatic cross check with schema.
    model can be created with any details. its the save() part that triggers an issue if the constraints are violated

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const User=require("./models/User");
 
 const dbConnect=require("./config/database");
 const app = express();
+app.use("/",express.json());
+
+app.post("/signup",async (req,res)=>{
+    console.log(req.body);
+    
+    try{
+        const user=new User(req.body);
+        await user.save();
+        res.send(("User Added Successfully"));
+        
+    }
+    catch(err){
+        console.error("something went wrong",err.message);
+        res.send(("something went wrong"));
+    }
+
+    
+
+
+});
 
 dbConnect().then(()=>{
     console.log("database connected successfully");
