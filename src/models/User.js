02c3ddc584c054043030df1236df946c4d11574b
Vagish <Vagishmlk@gmail.com>
2024-11-15 00:11:20 +0530

    updated schema checks so only correct data enters db

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,36 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const UserSchema=new mongoose.Schema({
-    firstName:{type:String},
-    lastName:{type:String},
-    email:{type:String},
-    password:{type:String},
-});
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            lowercase: true,
+            maxLength: 30,
+            required: true,
+        },
+        lastName: { type: String, lowercase: true, maxLength: 30 },
+        email: { type: String, lowercase: true, unique: true, required: true },
+        password: { type: String, required: true },
+        age: { type: Number, min: 18 },
+        gender: {
+            type: String,
+            validate(value) {
+                if (!["male", "female", "others"].includes(value)) {
+                    throw new Error("Gender data is not valid");
+                }
+            },
+        },
+        about: { type: String, default: "This is DEFAULT About. " },
+        skills: { type: [String] },
+        photoURL: {
+            type: String,
+            default:
+                "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw2ggkxKcRisToE8223z0WGe&ust=1731683774257000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjB4_OO3IkDFQAAAAAdAAAAABAJ",
+        },
+    },
+    { timestamps: true }
+);
 
-const User=new mongoose.model("User",UserSchema);
+const User = new mongoose.model("User", UserSchema);
 
-module.exports=User;
+module.exports = User;
