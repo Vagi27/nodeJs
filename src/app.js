0d951c0b8faaa7353df5d62a2e270b3fcaf95e2a
Vagish <Vagishmlk@gmail.com>
2024-11-16 00:06:12 +0530

    removed model, refactored for file change

--- src/app.js
+++ src/app.js
@@ -2,15 +2,14 @@ const express = require("express");
 const cookieParser=require("cookie-parser");
 
 const dbConnect = require("./config/database");
-const User = require("./models/User");
 const { authRouter } = require("./routes/auth");
-const { userRouter } = require("./routes/user");
+const { profileRouter } = require("./routes/profile");
 const app = express();
 
 app.use("/", express.json());
 app.use("/", cookieParser());
-app.use(authRouter);
-app.use(userRouter);
+app.use(authRouter); //equivalent to-> app.use("/",authRouter)
+app.use(profileRouter);
 
 dbConnect()
     .then(() => {
