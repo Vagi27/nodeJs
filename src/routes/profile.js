fe0dca457c4342835a2f59553e65b8c34e7e0984
Vagish <Vagishmlk@gmail.com>
2024-11-16 00:05:42 +0530

    profile router is more accurate to the api's function

---
+++ src/routes/profile.js
@@ -0,0 +1,25 @@
+const User = require("../models/User");
+const { userAuth } = require("../middlewares/userAuth");
+
+const profileRouter = require("express").Router();
+const safeData = "firstName lastName about age gender skills photoURL";
+
+profileRouter.get("/allusers", userAuth, async (req, res) => {
+    try {
+        const users = await User.find().select(safeData);
+        console.log(users);
+        res.send(users);
+    } catch (err) {
+        res.send("error Occurred:" + err.message);
+    }
+});
+profileRouter.get("/profile", userAuth, async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.user.id }).select(safeData);
+        res.json({ data: user });
+    } catch (err) {
+        res.status(400).send("cannot get profile " + err.message);
+    }
+});
+
+module.exports = { profileRouter };
