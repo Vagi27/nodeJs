51e12ef427fdede3dddfeb5d68bf7fa25597de7c
Vagish <Vagishmlk@gmail.com>
2024-11-16 21:59:41 +0530

    viewing connection requests API

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,25 @@
 const { userAuth } = require("../middlewares/userAuth");
+const connectionRequest = require("../models/connectionRequest");
 
-const userRouter=require("express").Router();
+const userRouter = require("express").Router();
 
-userRouter.get("/user/requests",userAuth,(req,res)=>{});
+userRouter.get("/user/requests", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const connectionRequests = await connectionRequest
+            .find({
+                toUserId: loggedInUser.id,
+                status: "interested",
+            })
+            .populate("fromUserId", "firstName lastName");
 
-module.exports={userRouter};
+        res.json({
+            message: `all requests to ${loggedInUser.firstName}`,
+            data: connectionRequests,
+        });
+    } catch (err) {
+        res.send("Couldn't get request: " + err.message);
+    }
+});
+
+module.exports = { userRouter };
