ee48a74a3e70c464304120df3bd8654469e2f07a
Vagish <Vagishmlk@gmail.com>
2024-11-17 08:35:37 +0530

    connection api correction + new feed API structure

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,9 @@
 const { userAuth } = require("../middlewares/userAuth");
 const connectionRequest = require("../models/connectionRequest");
-
+const User = require("../models/User");
 const userRouter = require("express").Router();
+
+const safeData = "firstName lastName about age gender skills photoURL";
 
 userRouter.get("/user/requests", userAuth, async (req, res) => {
     try {
@@ -25,21 +27,37 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
 
-        const connections = await connectionRequest.find({
-            $or: [
-                { fromUserId: loggedInUser.id, status: "accepted" },
-                { toUserId: loggedInUser.id, status: "accepted" },
-            ],
-        }).populate("fromUserId","firstName lastName");
-        if (!connections) {
-            throw new Error("No connections found");
-        }
+        const connections = await connectionRequest
+            .find({
+                $or: [
+                    { fromUserId: loggedInUser.id, status: "accepted" },
+                    { toUserId: loggedInUser.id, status: "accepted" },
+                ],
+            })
+            .populate("fromUserId", "firstName")
+            .populate("toUserId", "firstName");
+
+        const data = connections.map((row) => {
+            if (row.fromUserId._id.toString() === loggedInUser.id.toString()) {
+                return row.toUserId;
+            } else {
+                return row.fromUserId;
+            }
+        });
+
         res.json({
             message: `Connections of ${loggedInUser.firstName}`,
-            data: connections,
+            data,
         });
     } catch (err) {
-        res.send("Couldn't get connections: " + err.message);
+        res.status(400).send("Couldn't get connections: " + err.message);
+    }
+});
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+    try {
+       
+    } catch (err) {
+        res.status(400).send("Cannot get feed: " + err.message);
     }
 });
 
