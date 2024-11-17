b693e853edfa91be37cb9c4955feaa8566e1c069
Vagish <Vagishmlk@gmail.com>
2024-11-17 08:36:25 +0530

    feed api implementation

--- src/routes/user.js
+++ src/routes/user.js
@@ -55,7 +55,41 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 });
 userRouter.get("/user/feed", userAuth, async (req, res) => {
     try {
-       
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 2;
+        const skip = (page - 1) * limit;
+
+        const connectionRequests = await connectionRequest
+            .find({
+                $or: [
+                    { fromUserId: loggedInUser.id },
+                    { toUserId: loggedInUser.id },
+                ],
+            })
+            .select("fromUserId toUserId status");
+
+        const hideUsersFromFeed = new Set();
+
+        connectionRequests.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
+        /**
+         * Ensure new users who haven’t made connection requests don't see their own profile,
+         * -> in the feed by mandating a query parameter to exclude their ID.
+         * */
+        const usersOnFeed = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                { _id: { $ne: loggedInUser.id } },
+            ],
+        })
+            .select(safeData)
+            .skip(skip)
+            .limit(limit);
+        res.json({ message: "Feed!", data: usersOnFeed });
     } catch (err) {
         res.status(400).send("Cannot get feed: " + err.message);
     }
