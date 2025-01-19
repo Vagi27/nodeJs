6a7fc02148bf62f4758820312c2f301f8ef44698
Vagish <Vagishmlk@gmail.com>
2025-01-20 00:09:21 +0530

    invalid jwt is now sent with status 401 for corrent handling on frontend

--- src/middlewares/userAuth.js
+++ src/middlewares/userAuth.js
@@ -1,20 +1,18 @@
 const jwt = require("jsonwebtoken");
 const JWT_Secret_Key = "DevTinder@$123";
-const User=require("../models/User");
+const User = require("../models/User");
 
 const userAuth = async (req, res, next) => {
     try {
         const { token } = req.cookies;
         const user = jwt.verify(token, JWT_Secret_Key);
-        if (!user) {
-            console.log("invalid token");
-        }
-        const safeData = "firstName lastName email about age gender skills photoURL";
-        req.user = await User.findOne({_id:user.id}).select(safeData);
+        const safeData =
+            "firstName lastName email about age gender skills photoURL";
+        req.user = await User.findOne({ _id: user.id }).select(safeData);
         // console.log(req.user);
         next();
     } catch (err) {
-        res.send("Error: token is not valid!!");
+        return res.status(401).send("invalid token!");
     }
 };
 
